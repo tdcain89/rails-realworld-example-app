2b15ab461e6693c807d5bc2e7c1f2096d12d5c92
Tyler Cain <tyler.cain@revzilla.com>
2018-03-06 17:44:45 -0500

    add basic seeds, fix cors

--- config/application.rb
+++ config/application.rb
@@ -22,9 +22,12 @@ module Conduit
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
-    config.action_dispatch.default_headers = {
-      'Access-Control-Allow-Origin' => 'http://localhost:3001',
-      'Access-Control-Request-Method' => %w{GET POST OPTIONS}.join(",")
-    }
+
+    config.middleware.insert_before 0, "Rack::Cors" do
+      allow do
+        origins '*'
+        resource '*', :headers => :any, :methods => [:get, :post, :delete, :put, :patch, :options]
+      end
+    end
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+seed_user_1 = User.create!(username: "millz1", email: 'test.app-1@revzilla.com', password: 'thisisnotsecure8')
+
+(1..101).each do |i|
+  Article.create!(
+    user: seed_user_1,
+    title: "Test Article #{i}",
+    slug: "test-article-#{i}",
+    body: %Q(
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+    ),
+    description: "Lorem ipsum dolor sit amet"
+  )
+end
